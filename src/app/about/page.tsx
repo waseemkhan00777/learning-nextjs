4839aacdbc9bc45eb92575c5cc16bf733ae6eaf7
Waseem khan <wasim5388@gmail.com>
2024-09-22 17:08:02 +0500

    basic fetch

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,31 @@
+import Card from "@/components/card";
 import React from "react";
 
-const About = () => {
-  return <div>About Page</div>;
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  username: string;
+};
+
+const About = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+  return (
+    <div>
+      {users.map((user: User) => {
+        return (
+          <Card key={user.id}>
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h2>Username: {user.username}</h2>
+              <h3>Email: {user.email}</h3>
+            </div>
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
 
 export default About;
